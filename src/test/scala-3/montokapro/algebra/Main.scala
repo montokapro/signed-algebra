9deffa464b47a56f9952e0fa70d594ff08936992
Stephen Webber <montokapro@gmail.com>
2025-11-12 22:48:37 -0600

    Improve example description

--- src/test/scala-3/montokapro/algebra/Main.scala
+++ src/test/scala-3/montokapro/algebra/Main.scala
@@ -17,10 +17,29 @@ implicit val signedIntEncoder: Encoder[Signed[Set[Int]]] = Encoder.instance[Sign
   }
 }
 
-// Example
+// The following example
 //
 // > sbt
 // > Test/run "[[0, 1, 2], [[1, 2, 3], [2, 3, 4]]]"
+//
+// will return
+//
+// [
+//   [
+//     0,
+//     1
+//   ]
+// ]
+//
+// We can reduce this ourselves to check correctness
+//
+// [[0, 1, 2], [[1, 2, 3], [2, 3, 4]]]
+// by intersection
+// [[0, 1, 2], [[2, 3]]]
+// by double negation elimination
+// [[0, 1, 2], 2, 3]
+// by subtraction
+// [[0, 1]]
 @main def reduce(s: String) = println(
   decode[SignedTree[Int]](s) match {
     case Left(e) => e
